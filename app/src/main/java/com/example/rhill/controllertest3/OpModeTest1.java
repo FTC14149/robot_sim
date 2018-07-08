f4eb9b5abea203cc729f25b84d07986c14426687
codehenge <codehenge@gmail.com>
2018-07-08 09:57:03 -0700

    OpModeTest1 now has go-straight command.

--- app/src/main/java/com/example/rhill/controllertest3/OpModeTest1.java
+++ app/src/main/java/com/example/rhill/controllertest3/OpModeTest1.java
@@ -11,11 +11,21 @@ public class OpModeTest1 extends FakeOpMode {
     }
     public void init_loop() {
     }
+    int distance_to_go;
     public void loop() {
         //robot.LeftMotorTorque(gamepad1.left_stick_y);
         //robot.RightMotorTorque(gamepad1.right_stick_y);
-        robot.LeftMotorImpulse(gamepad1.left_stick_y);
-        robot.RightMotorImpulse(gamepad1.right_stick_y);
+        if(gamepad1.a) {
+            distance_to_go = 50;
+        }
+        if(distance_to_go > 0) {
+            robot.LeftMotorImpulse(-1);
+            robot.RightMotorImpulse(-1);
+            distance_to_go -= 1;
+        } else {
+            robot.LeftMotorImpulse(gamepad1.left_stick_y);
+            robot.RightMotorImpulse(gamepad1.right_stick_y);
+        }
     }
     public void stop() {
     }
