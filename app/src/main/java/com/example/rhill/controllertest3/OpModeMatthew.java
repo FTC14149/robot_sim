ad765a6c54347797d4963faaf33eef50c6511477
doonzan <doonzan@gmail.com>
2018-07-15 18:12:32 -0700

    turn to 90, compass measurements, & moving until touching

--- app/src/main/java/com/example/rhill/controllertest3/OpModeMatthew.java
+++ app/src/main/java/com/example/rhill/controllertest3/OpModeMatthew.java
@@ -8,10 +8,14 @@ package com.example.rhill.controllertest3;
 public class OpModeMatthew extends FakeOpMode {
     float speed;
     int count;
+    boolean im_busy_turning;
+    boolean run_away;
     public void init() {
         this.telemetry.AddData(this.getClass().getSimpleName(), "Started");
         speed = 1;
-        count = 30;
+        count = 0;
+        im_busy_turning = false;
+        run_away = false;
     }
     public void init_loop() {
     }
@@ -22,19 +26,44 @@ public class OpModeMatthew extends FakeOpMode {
         if (gamepad1.y) {
             speed = speed / 2;
         }
-        if (gamepad1.x) {
-            count = 30;
+        if (gamepad1.b) {
+           im_busy_turning = true;
         }
-        if(count > 0) {
-            // move robot forward
-            robot.LeftMotorTorque(-1);
-            robot.RightMotorTorque(-1);
-            count = count - 1;
+        if (im_busy_turning == true) {
+            if (robot.Compass() < 90) {
+                // turn robot right
+                robot.LeftMotorTorque(-1);
+                robot.RightMotorTorque(1);
+                count = count - 1;
+            } else {
+                if (robot.Compass() > 90) {
+                    // turn robot left
+                    robot.LeftMotorTorque(1);
+                    robot.RightMotorTorque(-1);
+                    count = count - 1;
+                } else {
+                   im_busy_turning = false;
+                }
+            }
         } else {
-            // move robot with controller
             robot.LeftMotorTorque(gamepad1.left_stick_y * speed);
             robot.RightMotorTorque(gamepad1.right_stick_y * speed);
         }
+        if (gamepad1.x){
+            run_away = true;
+        }
+        if (run_away == true) {
+            if (robot.frontSensorTouching == false) {
+                robot.LeftMotorTorque(-1);
+                robot.RightMotorTorque(-1);
+            } else {
+                run_away = false;
+            }
+        } else {
+            robot.LeftMotorTorque(gamepad1.left_stick_y * speed);
+            robot.RightMotorTorque(gamepad1.right_stick_y * speed);
+        }
+        this.telemetry.AddData("compass", String.valueOf(robot.Compass()));
     }
     public void stop() {
     }
