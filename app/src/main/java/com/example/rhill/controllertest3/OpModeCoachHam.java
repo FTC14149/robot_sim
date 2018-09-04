d76c0c0ca1a1adc49a9d83e5463c6b70448a54f9
kathyrader <krader1@san.rr.com>
2018-09-03 22:15:21 -0700

    x button move forward

--- app/src/main/java/com/example/rhill/controllertest3/OpModeCoachHam.java
+++ app/src/main/java/com/example/rhill/controllertest3/OpModeCoachHam.java
@@ -6,45 +6,104 @@ package com.example.rhill.controllertest3;
 
 import org.jbox2d.common.Vec2;
 
+import static java.lang.StrictMath.abs;
+
 public class OpModeCoachHam extends FakeOpMode {
+    public enum states {
+        IDLE,
+        ENTERLENGTH,
+        FWD,
+        TURN
+
+    }
     float speed = 1;
     int count;
     boolean magnetEnabled = false;
+    states mystate;
+    int newheading;
+    int turns = 0;
     public void init() {
         this.telemetry.AddData(this.getClass().getSimpleName(), "Started");
+        mystate = states.IDLE;
     }
     public void init_loop() {
     }
     public void loop() {
+        boolean forward = false;
         int heading = robot.Compass();
         Vec2 location = robot.Location();
-        this.telemetry.AddData("conpass:" + String.valueOf(heading), "location (" + String.valueOf(location.x) + "," + String.valueOf(location.y) + ")");
-        if(heading != 90)
-            robot.LeftMotorTorque(-1);
-        if(gamepad1.x & gamepad1.b) {
-            count = 30;
+        //this.telemetry.AddData("conpass:" + String.valueOf(heading), "location (" + String.valueOf(location.x) + "," + String.valueOf(location.y) + ")");
+
+
+
+        switch(mystate) {
+            case ENTERLENGTH:
+                count = 10;
+                mystate = states.FWD;
+                break;
+            case FWD:
+                if (count > 0) {
+                    robot.LeftMotorTorque(-1);
+                    robot.RightMotorTorque(-1);
+                    count = count -1;
+                }
+                else {
+                    newheading = (heading + 90) % 360;
+                    turns = turns+1;
+                    this.telemetry.AddData("conpass:" + String.valueOf(newheading), "location (" + String.valueOf(location.x) + "," + String.valueOf(location.y) + ")");
+                    mystate = states.TURN;
+                }
+                break;
+            case TURN:
+                if (turns == 5) {
+                    mystate = states.IDLE;}
+                if (abs(heading-newheading) < 2) {
+                    mystate = states.ENTERLENGTH;
+                } else {
+                    robot.LeftMotorTorque(-1);
+                }
+                break;
+            case IDLE:
+                robot.LeftMotorTorque(gamepad1.left_stick_y * speed);
+                robot.RightMotorTorque(gamepad1.right_stick_y * speed);
+                if (gamepad1.a) {
+                    mystate = states.ENTERLENGTH;
+                    turns = 0;
+                }
+                break;
         }
-        if (count > 0) {
-            robot.LeftMotorTorque(-1);
-            robot.RightMotorTorque(-1);
-            count = count -1;
-        } else {
-            robot.LeftMotorTorque(gamepad1.left_stick_y * speed);
-            robot.RightMotorTorque(gamepad1.right_stick_y * speed);
-        }
-        if (gamepad1.a) {
-            speed = 1.2f * speed;
-        }
-        if (gamepad1.b & gamepad1.y) {
-            speed = speed / 1.2f;
-        }
-        if(gamepad1.x) {
-            magnetEnabled = true;
-        }
-        if(gamepad1.y) {
-            magnetEnabled = false;
-        }
-        robot.EnableMagnet(magnetEnabled);
+
+        //if (heading != 270) {
+        //   robot.LeftMotorTorque(-1);
+        //}
+        //if (location.x != 5) {
+         //   robot.LeftMotorTorque(-1);
+         //   robot.RightMotorTorque(-1);
+        //}
+//        if(gamepad1.x & gamepad1.b) {
+//            count = 30;
+//        }
+//        if (count > 0) {
+//            robot.LeftMotorTorque(-1);
+//            robot.RightMotorTorque(-1);
+//            count = count -1;
+//        } else {
+//            robot.LeftMotorTorque(gamepad1.left_stick_y * speed);
+//            robot.RightMotorTorque(gamepad1.right_stick_y * speed);
+//        }
+//        if (gamepad1.a) {
+//            speed = 1.2f * speed;
+//        }
+//        if (gamepad1.b & gamepad1.y) {
+//            speed = speed / 1.2f;
+//        }
+//        if(gamepad1.x) {
+//            magnetEnabled = true;
+//        }
+//        if(gamepad1.y) {
+//            magnetEnabled = false;
+//        }
+//        robot.EnableMagnet(magnetEnabled);
 
     }
 
